68db3d17864e568c87a2196d9549427788bf898a
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-06-07 20:16:38 +0200

    :construction: DataTable: Filtering component and typing

--- src/pages/EmployeeList/index.tsx
+++ src/pages/EmployeeList/index.tsx
@@ -1,7 +1,9 @@
+import { useState, useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import DataTable from 'react-data-table-component'
-import columns from '../../components/DataTable'
+import columns from '../../components/DataTable/Columns'
+import FilterComponent from '../../components/DataTable/Filtering'
 import '../EmployeeList/EmployeeList.scss'
 
 type Employee = {
@@ -22,15 +24,45 @@ function EmployeeList() {
   const { employeesList } = useSelector(
     (state: { employees: { employeesList: Array<Employee> } }) => state.employees
   )
-  
+
+  const [filterText, setFilterText] = useState('');
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+  const filteredItems = employeesList.filter(
+    item =>
+      item.last_name && item.last_name.toLowerCase().includes(filterText.toLowerCase()) ||
+      item.first_name && item.first_name.toLowerCase().includes(filterText.toLowerCase()) ||
+      item.adress_city && item.adress_city.toLowerCase().includes(filterText.toLowerCase())
+    ,
+  )
+
+  const subHeaderComponentMemo = useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText('');
+      }
+    }
+
+    return (
+      <FilterComponent
+        filterText={filterText}
+        onFilter={(e) => setFilterText(e.currentTarget.value)}
+        onClear={handleClear}
+      />
+    )
+  }, [filterText, resetPaginationToggle])
+
   return (
     <main>
       <h2>Current Employees</h2>
-        <DataTable
-          columns={columns}
-          data={employeesList}
-          pagination
-        />
+      <DataTable
+        columns={columns}
+        data={filteredItems}
+        pagination
+        paginationResetDefaultPage={resetPaginationToggle}
+        subHeader
+        subHeaderComponent={subHeaderComponentMemo}
+      />
       <footer>
         <Link to='/'>Home</Link>
       </footer>
