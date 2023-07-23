50a1e50b4bb23e8cb1a81d418b4d95c5b2236064
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-07-23 18:52:03 +0200

    :lipstick::zap: Resize img, srcSet multiple size for performance improvment

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
-import heroImg from '../../assets/img/cytonn-photography-n95VMLxqM2I-unsplash.jpg'
-import wealthHealthLogo from '../../assets/wealthHealthLogo.jpg'
+import heroImgXL from '../../assets/img/cytonn-photography-n95VMLxqM2I-u_1200.jpg'
+import heroImgL from '../../assets/img/cytonn-photography-n95VMLxqM2I-u_992.jpg'
+import heroImgM from '../../assets/img/cytonn-photography-n95VMLxqM2I-u_768.jpg'
+import heroImgS from '../../assets/img/cytonn-photography-n95VMLxqM2I-u_576.jpg'
+import heroImgXS from '../../assets/img/cytonn-photography-n95VMLxqM2I-u_360.jpg'
+
+import wealthHealthLogo from '../../assets/wealthHealthLogo.svg'
 import '../Home/Home.scss'
 
 function Home() {
@@ -7,10 +12,22 @@ function Home() {
     <main>
       <h2>Bienvenue</h2>
       <div className='main-hero'>
-        <img className='main-hero-img' src={heroImg} alt='A handshake between two collegues' />
+        <img
+          loading='lazy'
+          className='main-hero-img'
+          srcSet={`${heroImgL} 992w,
+                   ${heroImgM} 768w,
+                   ${heroImgS} 576w,
+                   ${heroImgXS} 360w,`}
+          sizes="(max-width: 992px) 992px,
+                 (max-width: 768px) 768px,
+                 (max-width: 576px) 576px,
+                 (max-width: 360px) 360px,
+                 1200px"
+          src={heroImgXL} alt='A handshake between two collegues' />
       </div>
       <div className='main-company'>
-        <img src={wealthHealthLogo} alt='The Wealth Health company logo' />
+        <img loading='lazy' src={wealthHealthLogo} alt='The Wealth Health company logo' />
       </div>
     </main>
   )
