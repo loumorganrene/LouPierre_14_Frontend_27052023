b9a029018c36dce9ea37a27159b32b69064d9206
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-07-25 19:47:53 +0200

    :wheelchair: Add aria-label on h1 link for accessibility

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,11 +4,11 @@ import '../Nav/Nav.scss'
 function Nav() {
     return (
         <nav className="main-nav">
-            <Link to='/' className="main-nav-logo">
-                <h1>
+
+            <h1>
+                <Link to='/' className="main-nav-logo" aria-label='HRnet logo'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="105" height="39" viewBox="0 0 105 39">
                         <text
-                            id="HRnet"
                             transform="translate(0 31)"
                             fill="#145495"
                             font-size="32"
@@ -18,8 +18,8 @@ function Nav() {
                             <tspan x="0" y="0">HR</tspan><tspan y="0" fill="#74b9ff">net</tspan>
                         </text>
                     </svg>
-                </h1>
-            </Link>
+                </Link>
+            </h1>
 
             <ul className='main-nav-links'>
                 <li>
