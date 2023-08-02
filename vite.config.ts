a9287886db6726d5954cd7a94ce9fe3664202cf8
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-08-02 04:25:59 +0200

    :zap: Uninstall SSR plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
-import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ssr({ prerender: true }),
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
