7a258bd7248461d838cb48b5c229b7f9bc842eaf
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-08-02 04:20:12 +0200

    :zap: Testing SSR for pre-rendering pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
+import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ssr({ prerender: true }),
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
